47e4d1555956b6f65fbd56cf71e20c238e9207d8
Ayush Yadav <sidharth197502@gmail.com>
2024-07-31 08:52:11 +0530

    made it responsive.

--- src/Components/TotalScore.jsx
+++ src/Components/TotalScore.jsx
@@ -34,4 +34,36 @@ const Scorecontainer = styled.div`
     font-weight: 500;
     line-height: 36px;
   }
+
+    @media (max-width: 768px) {
+    h1 {
+      font-size: 80px;
+      width: 50px;
+      height: 90px;
+      line-height: 120px;
+    }
+
+    p {
+      font-size: 20px;
+      width: 120px;
+      height: 30px;
+      line-height: 30px;
+    }
+  }
+
+  @media (max-width: 480px) {
+    h1 {
+      font-size: 60px;
+      width: 40px;
+      height: 70px;
+      line-height: 90px;
+    }
+
+    p {
+      font-size: 18px;
+      width: 100px;
+      height: 25px;
+      line-height: 25px;
+    }
+  }
 `;
